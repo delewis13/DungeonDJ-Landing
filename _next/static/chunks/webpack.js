899d340774eacacaa10a4e0b7b59774e93d41d2f
Daniel Lewis <danlewis@canva.com>
2021-08-31 00:05:05 +1000

    Updates

--- _next/static/chunks/webpack.js
+++ _next/static/chunks/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "68aa399fe97a58658e53";
+/******/ 	var hotCurrentHash = "38b9eba2c442df4dd0b6";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
