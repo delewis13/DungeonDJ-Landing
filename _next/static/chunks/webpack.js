72757c143635a519aac214910f7e530cbccc2c87
Daniel Lewis <danlewis@canva.com>
2022-01-30 19:38:03 +1100

    Updates

--- _next/static/chunks/webpack.js
+++ _next/static/chunks/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "34344f0c892950b6cf4d";
+/******/ 	var hotCurrentHash = "42eccf8881a7dca2604e";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
